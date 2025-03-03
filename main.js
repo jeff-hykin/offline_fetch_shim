8717bd10bf5e7ef050201081982eab0447e25d40
Jeff Hykin <jeff.hykin@gmail.com>
2025-03-03 13:29:35 -0600

    add docs to monkeyPatch

--- main.js
+++ main.js
@@ -2,6 +2,43 @@
     // (yeah it says "dont use" but whatever)
     // https://w3c.github.io/web-performance/specs/HAR/Overview.html#sec-object-types-params
 
+/**
+ * patch a method, without breaking "this" behavior, similar to a decorator
+ *
+ * @example
+ * ```js
+ * // Example:
+ * const obj = {
+ *     thisName: 'Bob',
+ *     greet(name) {
+ *         return `Hello, ${name}!\n - from: ${this.thisName}`;
+ *     }
+ * };
+ *
+ * // Wrap the greet function to log the greeting before returning it
+ * monkeyPatch(obj, 'greet', (originalGreet) => {
+ *     return function(...args) {
+ *         console.log(`Calling greet with args: ${args}`);
+ *         const result = originalGreet(...args);
+ *         console.log(`Result: ${result}`);
+ *         return result;
+ *     };
+ * });
+ *
+ * // After patching, the greet method logs the arguments and result
+ * obj.greet('Alice');
+ * // Console:
+ * // Calling greet with args: [ 'Alice' ]
+ * // Result: Hello, Alice!
+ * // - from: Bob
+ * ```
+ * @param {Object} object - The object whose method is to be patched.
+ * @param {string} attrName - The name of the method to patch.
+ * @param {Function} createNewFunction - A function that takes the original function and 
+ *                                       returns a new function that will replace the original.
+ * @throws {Error} If the specified method does not exist in the object or its prototype chain.
+ * 
+ */ 
 export function monkeyPatch(object, attrName, createNewFunction) {
     let prevObj = null
     while (!Object.getOwnPropertyNames(object).includes(attrName)) {
